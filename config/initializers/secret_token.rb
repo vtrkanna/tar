5a82df4bef8466528a376dd7305668d21238f4f8
kanna <vtrkanna@yahoo.com>
2013-08-03 11:17:45 +0530

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tra::Application.config.secret_key_base = 'b1b908766f9c128d1b066b9d45ac1443710ca8821fa8c4b2352322ca9d6df4d0d99ccface76320ab30997688c54506bb355ade2a7ef4411ca6cb9bf8ace6ea97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tra::Application.config.session_store :cookie_store, key: '_tra_session'
